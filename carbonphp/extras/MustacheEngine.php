15898f83c203b966aff3eeff0be4f0844fbde8b4
richardtmiles <richard@miles.systems>
2024-06-20 11:55:03 -0600

    no catching

--- carbonphp/extras/MustacheEngine.php
+++ carbonphp/extras/MustacheEngine.php
@@ -14,11 +14,17 @@ class MustacheEngine extends Mustache_Engine
 
             ThrowableHandler::stop();
 
-            error_reporting(ThrowableHandler::$level ^ (E_NOTICE | E_WARNING | E_DEPRECATED));
+            $oldLevel =ThrowableHandler::$level;
+
+            ThrowableHandler::$level ^= (E_NOTICE | E_WARNING | E_DEPRECATED);
+
+            error_reporting(ThrowableHandler::$level);
 
             // @note if the Mustache_Engine fails, then tries to re-load itsself the message will say "Class "Mustache_Engine" not found"
             // This is very deceptive as the class is found, but failing to be loaded via compile time error. E_DEPRECATED is the first sign
             parent::__construct($options);
+
+            ThrowableHandler::$level = $oldLevel;
 
             ThrowableHandler::start();
 
@@ -37,11 +43,19 @@ class MustacheEngine extends Mustache_Engine
 
             ThrowableHandler::stop();
 
-            error_reporting(ThrowableHandler::$level ^ (E_NOTICE | E_WARNING | E_DEPRECATED));
+            $oldLevel =ThrowableHandler::$level;
+
+            ThrowableHandler::$level ^= (E_NOTICE | E_WARNING | E_DEPRECATED);
+
+            error_reporting(ThrowableHandler::$level);
 
             // @note if the Mustache_Engine fails, then tries to re-load itsself the message will say "Class "Mustache_Engine" not found"
             // This is very deceptive as the class is found, but failing to be loaded via compile time error. E_DEPRECATED is the first sign
             $result = parent::render($template, $context);
+
+            ThrowableHandler::$level = $oldLevel;
+
+            error_reporting(ThrowableHandler::$level);
 
             ThrowableHandler::start();
 
